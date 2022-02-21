9169002bd38d2eb34a47af2dabe2c061686b1eda
Adrianfmc <adrianfmc@gmail.com>
2022-02-21 17:26:40 -0600

    vistas para calcular ponderaciones

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -29,9 +29,9 @@ class Alumnx(models.Model):
 
 class Primer_parcial(models.Model):
     nombre = models.ForeignKey('Alumnx', on_delete=models.CASCADE)
-    calif_mono = models.DecimalField(max_digits=2, decimal_places=1)
-    calif_expo_1er = models.DecimalField(max_digits=2, decimal_places=1)
-    participacion_1er = models.DecimalField(max_digits=2, decimal_places=1)
+    calif_mono = models.DecimalField(max_digits=3, decimal_places=1)
+    calif_expo_1er = models.DecimalField(max_digits=3, decimal_places=1)
+    participacion_1er = models.DecimalField(max_digits=3, decimal_places=1)
 
 
     def __str__(self):
@@ -39,18 +39,18 @@ class Primer_parcial(models.Model):
 
 class Segundo_parcial(models.Model):
     nombre = models.ForeignKey('Alumnx', on_delete=models.CASCADE)
-    calif_ensay_2do = models.DecimalField(max_digits=2, decimal_places=1, help_text="Ingrese la calificación del ensayo (2do parcial)")
-    calif_expo_2do = models.DecimalField(max_digits=2, decimal_places=1, help_text="Ingrese la calificación de la exposición (2do parcial)")
-    participacion_2do = models.IntegerField(default=0)
+    calif_ensay_2do = models.DecimalField(max_digits=3, decimal_places=1, help_text="Ingrese la calificación del ensayo (2do parcial)")
+    calif_expo_2do = models.DecimalField(max_digits=3, decimal_places=1, help_text="Ingrese la calificación de la exposición (2do parcial)")
+    participacion_2do = models.DecimalField(max_digits=3, decimal_places=1)
 
     def __str__(self):
         return self.nombre
 
 class Final(models.Model):
     nombre = models.ForeignKey('Alumnx', on_delete=models.CASCADE)
-    calif_ensay_final = models.DecimalField(max_digits=2, decimal_places=1, help_text="Ingrese la calificación del ensayo (3er parcial)")
-    calif_expo_final = models.DecimalField(max_digits=2, decimal_places=1, help_text="Ingrese la calificación de la exposición (3er parcial)")
-    participacion_final = models.IntegerField(default=0)
+    calif_ensay_final = models.DecimalField(max_digits=3, decimal_places=1, help_text="Ingrese la calificación del ensayo (3er parcial)")
+    calif_expo_final = models.DecimalField(max_digits=3, decimal_places=1, help_text="Ingrese la calificación de la exposición (3er parcial)")
+    participacion_final = models.DecimalField(max_digits=3, decimal_places=1)
 
     def __str__(self):
             return self.nombre
--- estudiantes/views.py
+++ estudiantes/views.py
@@ -4,6 +4,7 @@ from .forms import AlumnxForm, Primer_parcialForm, Segundo_parcialForm, FinalFor
 from django.contrib import messages
 from django.db.models.functions import Coalesce
 from decimal import Decimal
+from django.db.models import F
 
 
 def lista_alumnx(request):
@@ -44,7 +45,10 @@ def eliminar_alumnx(request, pk):
     return redirect('lista_alumnx')
 
 def lista_calif1erpar(request):
-    primer_parcials = Primer_parcial.objects.order_by('nombre')
+    primer_parcials = Primer_parcial.objects.annotate(calif_mono60=F('calif_mono')*Decimal(.6),
+                                                    calif_expo_1er10=F('calif_expo_1er')*Decimal(.1),
+                                                    participacion_1er30=F('participacion_1er')*Decimal(.3),
+                                                    promedio_1erparcial=(F('calif_mono')*Decimal(.6))+(F('calif_expo_1er')*Decimal(.1))+(F('participacion_1er')*Decimal(.3))).order_by('nombre')
     return render(request, 'estudiantes/lista_calif1erpar.html', {'primer_parcials':primer_parcials})
 
 def primer_parcial(request):
